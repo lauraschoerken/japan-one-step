ef1eca4b54fc25d36ad7443e9297e67569483032
Laura Schoerken <Lauraschorken@gmail.com>
2023-07-15 16:48:16 +0100

    feat: create mock decks data for tesing

--- src/components/Decks/containers/Decks.tsx
+++ src/components/Decks/containers/Decks.tsx
@@ -1,4 +1,27 @@
+import { Card, Decks } from 'models/Deck'
 import DecksComponent from '../components/DecksComponent'
+
+const card1: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+const card2: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+const card3: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+const card4: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+const card5: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+const card6: Card = { id: '1', japanese_value: 'あ', romanji_value: 'a' }
+
+const decks: Decks = {
+	hiragana: {
+		name: 'hiragana',
+		cards: [card1, card2],
+	},
+	katakana: {
+		name: 'katakana',
+		cards: [card3, card4],
+	},
+	kanji: {
+		name: 'kanji',
+		cards: [card5, card6],
+	},
+}
 
 const Decks = () => {
 	return <DecksComponent />
