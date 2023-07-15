7ceb26cf9b6985691dff7b666bd37d625082674f
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 11:01:41 +0100

    WIP: Trying store and usage

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from 'stores/hooks/hooks'
+import { setUser } from 'stores/reducers/user'
+import { setUsers } from 'stores/reducers/users'
+import { User } from 'models/User'
+import { setDecks } from 'stores/reducers/decks'
 
 function App() {
 	const [count, setCount] = useState(0)
+	const dispatch = useAppDispatch()
+	const user = useAppSelector((state) => state.user.value)
+	const users = useAppSelector((state) => state.users.value)
+
+	const laura: User = { id: '123-asd', name: 'Laura', email: 'lauraschorken@gmail.com' }
+	const david: User = { id: '456-qwe', name: 'David', email: 'davidhormigafonso@gmail.com' }
+
+	useEffect(() => {
+		dispatch(setUser(laura))
+		dispatch(setUsers([laura, david]))
+		dispatch(
+			setDecks({
+				hiragana: { name: 'hiragana', cards: [] },
+				katakana: { name: 'katakana', cards: [] },
+				kanji: { name: 'kanji', cards: [] },
+			})
+		)
+	}, [])
+
+	console.log('user =>', user)
+	console.log('users =>', users)
 
 	return (
 		<>
