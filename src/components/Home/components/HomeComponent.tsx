b25c8088ab0fa2e33a3e36ae803e0fadd81cff14
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 16:48:23 +0100

    fix: use Card in home component

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -1,6 +1,7 @@
 import Header from 'components/Header/containers/Header'
 import './HomeComponent.scss'
 import Decks from 'components/Decks/containers/Decks'
+import Card from 'components/Card/containers/Card'
 
 const HomeComponent = () => {
 	return (
@@ -13,7 +14,7 @@ const HomeComponent = () => {
 					<Decks />
 				</div>
 				<div className='cards'>
-					<h1>Cards</h1>
+					<Card />
 				</div>
 			</div>
 		</div>
