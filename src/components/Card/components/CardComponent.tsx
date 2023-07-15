9c1adeb2f391187edfbc29342c305091ad9ff03a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 17:54:12 +0100

    feat: template for card

--- src/components/Card/components/CardComponent.tsx
+++ src/components/Card/components/CardComponent.tsx
@@ -1,9 +1,41 @@
+import CardButton from './CardButton'
 import './CardComponent.scss'
+export interface Difficulty {
+	color: string
+	name: string
+}
 
 const CardComponent = () => {
+	const difficulties: Difficulty[] = [
+		{
+			name: 'easy',
+			color: '#069C56',
+		},
+		{
+			name: 'medium',
+			color: '#FF980E',
+		},
+		{
+			name: 'hard',
+			color: '#FF681E',
+		},
+		{
+			name: 'not_guessed',
+			color: '#D3212C',
+		},
+	]
+
 	return (
-		<div>
-			<h1>Card</h1>
+		<div className='card-main'>
+			<div className='show-value'>
+				<h2>あ</h2>
+			</div>
+			<input className='guess-value' type='text' />
+			<div className='buttons'>
+				{difficulties.map((difficulty) => (
+					<CardButton difficulty={difficulty} />
+				))}
+			</div>
 		</div>
 	)
 }
