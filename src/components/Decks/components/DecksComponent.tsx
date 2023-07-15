3305d64794f128bc7f28ba3acd98f12247ea0797
Laura Schoerken <Lauraschorken@gmail.com>
2023-07-15 18:59:35 +0100

    feat: add deck list

--- src/components/Decks/components/DecksComponent.tsx
+++ src/components/Decks/components/DecksComponent.tsx
@@ -1,16 +1,39 @@
-import { Decks } from 'models/Deck'
+import { Deck, DeckName, Decks } from 'models/Deck'
 import './DecksComponent.scss'
+import { useState } from 'react'
 
 interface Props {
 	decks: Decks
 }
 
 const DecksComponent: React.FC<Props> = (props) => {
+	const [selected, setSelected] = useState<DeckName>('hiragana')
+	const active = 'active'
+	const desactive = 'desactive'
+
 	const { decks } = props
 	console.log('decks =>', decks)
+	const onClickDecks = (deckRecived: DeckName) => {
+		setSelected(deckRecived)
+	}
+
 	return (
-		<div>
-			<h1>Deck</h1>
+		<div className='decks'>
+			<div
+				className={`deck ${selected === 'hiragana' ? 'selected' : ''}`}
+				onClick={() => onClickDecks('hiragana')}>
+				あ
+			</div>
+			<div
+				className={`deck ${selected === 'katakana' ? 'selected' : ''}`}
+				onClick={() => onClickDecks('katakana')}>
+				ア
+			</div>
+			<div
+				className={`deck ${selected === 'kanji' ? 'selected' : ''}`}
+				onClick={() => onClickDecks('kanji')}>
+				日本
+			</div>
 		</div>
 	)
 }
