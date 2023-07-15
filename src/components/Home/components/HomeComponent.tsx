ab490f36e05129dc94268503d1d56e26348cfb24
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 15:41:28 +0100

    feat: create base Home Component
    Co-authored-by: Laura Schörken <lauraschoerken@users.noreply.github.com>

---
+++ src/components/Home/components/HomeComponent.tsx
@@ -0,0 +1,9 @@
+const HomeComponent = () => {
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	)
+}
+
+export default HomeComponent
---
+++ src/components/Home/containers/Home.tsx
@@ -0,0 +1,7 @@
+import HomeComponent from '../components/HomeComponent'
+
+const Home = () => {
+	return <HomeComponent />
+}
+
+export default Home
