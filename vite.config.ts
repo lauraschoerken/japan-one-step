8da6db0196c0a2af8fa46af6b553af24fdbfe6b0
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 10:55:59 +0100

    chore: add relative routing for imports
    Co-authored-by: Laura Schörken <lauraschoerken@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import react from '@vitejs/plugin-react'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 
@@ -6,6 +8,16 @@ import eslint from 'vite-plugin-eslint'
 export default defineConfig({
 	plugins: [react(), eslint()],
 	resolve: {
-		alias: {},
+		alias: {
+			assets: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/assets'),
+			components: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/components'),
+			constants: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/constants'),
+			hooks: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/hooks'),
+			models: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/models'),
+			services: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/services'),
+			utils: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/utils'),
+			stores: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/stores'),
+			mock: path.resolve(dirname(fileURLToPath(import.meta.url)), './src/mock'),
+		},
 	},
 })
