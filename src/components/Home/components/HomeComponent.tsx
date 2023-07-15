6e85681c8b190dc3065768add28e72b03cec64df
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-07-15 16:47:19 +0100

    styles: add home basic styles
    Co-authored-by: Laura Schörken <lauraschoerken@users.noreply.github.com>

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -1,9 +1,21 @@
+import Header from 'components/Header/containers/Header'
 import './HomeComponent.scss'
+import Decks from 'components/Decks/containers/Decks'
 
 const HomeComponent = () => {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className='main'>
+			<div className='header'>
+				<Header />
+			</div>
+			<div className='body'>
+				<div className='decks'>
+					<Decks />
+				</div>
+				<div className='cards'>
+					<h1>Cards</h1>
+				</div>
+			</div>
 		</div>
 	)
 }
