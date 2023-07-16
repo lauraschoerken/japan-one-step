57270388827ff529c10482d0ac04b3c6831afbba
Laura Schoerken <Lauraschorken@gmail.com>
2023-07-16 16:25:59 +0100

    feat: add decksAndSimbol constant

---
+++ src/utils/constants.tsx
@@ -0,0 +1,7 @@
+import { DeckSimbol } from 'models/Deck'
+
+export const decksAndSimbol: DeckSimbol[] = [
+	{ name: 'hiragana', simbol: 'あ' },
+	{ name: 'katakana', simbol: 'ア' },
+	{ name: 'kanji', simbol: '日本' },
+]
