305e4e40a235cfb8bd6c79b8b8f2d6d8370c1095
Laura Schoerken <Lauraschorken@gmail.com>
2023-07-16 16:46:41 +0100

    feat: declare new constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,15 @@
-import { DeckSimbol } from 'models/Deck'
+import { DeckSymbol } from 'models/Deck'
 
-export const decksAndSimbol: DeckSimbol[] = [
-	{ name: 'hiragana', simbol: 'あ' },
-	{ name: 'katakana', simbol: 'ア' },
-	{ name: 'kanji', simbol: '日本' },
+export const hiragana = 'hiragana'
+export const katakana = 'katakana'
+export const kanji = 'kanji'
+
+export const hiragana_symbol = 'あ'
+export const katakana_symbol = 'ア'
+export const kanji_symbol = '日本'
+
+export const decksAndSymbol: DeckSymbol[] = [
+	{ name: hiragana, symbol: hiragana_symbol },
+	{ name: katakana, symbol: katakana_symbol },
+	{ name: kanji, symbol: kanji_symbol },
 ]
