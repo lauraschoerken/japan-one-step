47d5eac69159f5a52b64d9c0d5fff5941e249d1e
Laura Schoerken <Lauraschorken@gmail.com>
2023-10-14 13:20:57 +0100

    feat: checks for when romanji is a []

--- src/components/Card/components/CardComponent.tsx
+++ src/components/Card/components/CardComponent.tsx
@@ -1,7 +1,7 @@
 import { Card } from 'models/Deck'
-// import CardButton from './CardButton'
+import CardButton from './CardButton'
 import './CardComponent.scss'
-// import { difficulties } from 'utils/difficulties'
+import { difficulties } from 'utils/difficulties'
 import * as Yup from 'yup'
 import { useFormik } from 'formik'
 import { useTranslation } from 'react-i18next'
@@ -21,12 +21,25 @@ const CardComponent: React.FC<Props> = (props) => {
 	const [guessedValue, setGuessedValue] = useState('')
 	const [card, setCard] = useState(currentCard)
 	const { t } = useTranslation()
-	const showValue = card.reverse
-		? card.romanji_value.trim().toLowerCase()
-		: card.japanese_value.trim().toLowerCase()
-	const guessValue = card.reverse
-		? card.japanese_value.trim().toLowerCase()
-		: card.romanji_value.trim().toLowerCase()
+
+	let showValue
+	let guessValue
+
+	if (typeof card.romanji_value === 'string') {
+		showValue = card.reverse
+			? card.romanji_value.trim().toLowerCase()
+			: card.japanese_value.trim().toLowerCase()
+		guessValue = card.reverse
+			? card.japanese_value.trim().toLowerCase()
+			: card.romanji_value.trim().toLowerCase()
+	} else {
+		showValue = card.reverse
+			? card.romanji_value.join(' / ')
+			: card.japanese_value.trim().toLowerCase()
+		guessValue = card.reverse
+			? card.japanese_value.trim().toLowerCase()
+			: card.romanji_value.join(' / ')
+	}
 
 	useEffect(() => {
 		setCard(currentCard)
@@ -70,33 +83,36 @@ const CardComponent: React.FC<Props> = (props) => {
 				autoFocus
 				type='text'
 				value={formik.values.guess_value}
-				onChange={(e) => formik.setFieldValue('guess_value', e.currentTarget.value)}
+				onChange={(e) => {
+					formik.setFieldValue('guess_value', e.currentTarget.value)
+				}}
 				onKeyDown={handleKeyPress}
 			/>
-			{/* TODO: COMMENTED FOR MVP RELEASE */}
 			{guessedValue ? (
-				// <React.Fragment>
-				// 	<div className='guessed-value'>
-				// 		<p>
-				// 			{guessedValue === guessValue ? '✅' : '❌'}
-				// 			{guessValue}
-				// 		</p>
-				// 	</div>
-				// 	<div className='buttons'>
-				// 		{difficulties.map((difficulty, index) => (
-				// 			<CardButton difficulty={difficulty} key={index} onValidate={onValidate} />
-				// 		))}
-				// 	</div>
-				// </React.Fragment>
-				<div className='guessed-value' style={{ flexDirection: 'column' }}>
-					<p>
-						{guessedValue === guessValue ? '✅' : '❌'}
-						{guessValue}
-					</p>
-					<button className='submit' type='submit' onClick={onValidate}>
-						Next
-					</button>
-				</div>
+				<React.Fragment>
+					<div className='guessed-value'>
+						{!guessValue.includes('/') ? (
+							<p>
+								{guessedValue === guessValue ? '✅' : '❌'}
+								{guessValue}
+							</p>
+						) : (
+							<p>
+								{guessValue.split(' / ').map((part, index) => (
+									<span key={index}>
+										{part === guessedValue ? '✅' : '❌'}
+										{part}
+									</span>
+								))}
+							</p>
+						)}
+					</div>
+					<div className='buttons'>
+						{difficulties.map((difficulty, index) => (
+							<CardButton difficulty={difficulty} key={index} onValidate={onValidate} />
+						))}
+					</div>
+				</React.Fragment>
 			) : (
 				<div className='guessed-value'>
 					<button className='submit' type='submit' onClick={() => formik.handleSubmit()}>
